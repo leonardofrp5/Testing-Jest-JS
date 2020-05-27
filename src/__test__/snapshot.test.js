4f055c4728f440ea5b1674fdc8165a89f0f4a5f4
leonardofrp5 <leonardofrp5@gmail.com>
2020-05-26 22:24:56 -0500

    learnd and used snapshot with diferents values like a changed date in each test and other test is same values in test

--- src/__test__/snapshot.test.js
+++ src/__test__/snapshot.test.js
@@ -5,4 +5,20 @@ describe('test whit snapshot', () => {
   it('test request', () => {
     expect(getCharacter(rick)).toMatchSnapshot();
   });
+
+  it('fails test', () => {
+    const user = {
+      createAt: new Date(),
+      id: Math.floor(Math.random() * 20),
+    }
+    expect(user).toMatchSnapshot();
+  });
+
+  it('changed value in snapshot', () => {
+    const user = {
+      id: Math.floor(Math.random() * 20),
+      name: 'Leo Romero'
+    }
+    expect(user).toMatchSnapshot({id: expect.any(Number)})
+  });
 });
